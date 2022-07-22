1b14583dfadc1464ceb192de7ea2a91febd51395
mridle582 <mridle582@gmail.com>
2022-07-21 23:09:20 -0400

    set up post req routes

--- app.js
+++ app.js
@@ -7,9 +7,20 @@ const request = require ("request");
 const app = express();
 
 app.use(express.static("public"));
+app.use(bodyPaser.urlencoded({
+    extended: true
+}));
 
 app.get("/", (req, res)=>{
     res.sendFile(`${__dirname}/signup.html`);
+});
+
+app.post("/", (req, res) => {
+    console.log(req.body);
+    var firstName = req.body.fName;
+    var lastName = req.body.lName
+    var email = req.body.email;
+    console.log(firstName, lastName, email);
 });
 
 
